90c99281b11be48439f6fcd12b451f544d3f393d
Sean <141945444+SeanMikee@users.noreply.github.com>
2025-07-19 12:46:28 +0530

    Update first.py

--- first.py
+++ first.py
@@ -9,3 +9,29 @@ print(f"Hello {first_name}, your favourite food is {food}. Right? and your email
 age = 23
 quantity = 3
 print(f"You are {age} years old. You are buying {quantity} items.")
+# x = y = z = 'Banana'
+
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ['banana','orange','grapes']
+# x,y,z = fruits
+
+# print(x)
+# print(y)
+# print(z)
+
+x = ['hello','python'] #list
+y = ('hello','python') #tuple
+z = {'hello','python'} #set
+w = {'name': 'John', 'age':20} #dict
+
+print(type(w))
+
+a = '''lorem
+helo
+hal
+halo
+rdr2''' #this is a multiline text
+print(a)
